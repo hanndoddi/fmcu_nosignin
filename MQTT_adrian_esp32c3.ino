598664d5c16733a40d01e8c15e2ab447532f7479
Þórarinn Bjartur Breiðfjörð <hanndoddi@gmail.com>
2024-01-16 12:32:25 +0000

    Create MQTT_adrian_esp32c3.ino

---
+++ MQTT_adrian_esp32c3.ino
@@ -0,0 +1,203 @@
+////////////////////////////////////////////////////////////////////////////////
+// FMCU Button code
+// By Fran Sanchez (The Beach Lab)
+// January 2022
+//
+// IDE:
+//   Arduino 2.0.0
+// Platform:
+//   esp32 2.0.5 - https://github.com/espressif/arduino-esp32
+// Board:
+//   XIAO_ESP32C3
+// Libraries:
+//   AsyncMQTT_ESP32 v
+////////////////////////////////////////////////////////////////////////////////
+
+// Includes
+
+#include <WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// The things you have to modify
+const char id[] = "";            // your fablabs.io lab id 
+
+const int buttonPin = 10;           // the number of the pushbutton pin GPIO 10 ESP32
+const int ledPin = 2;               // the number of the LED pin  Internal LED 
+int buttonState = 0;                // variable for reading the pushbutton status
+
+#define WLAN_SSID ""        // your wifi name
+#define WLAN_PASS ""    // your wifi password
+////////////////////////////////////////////////////////////////////////////////
+
+
+// MQTT Settings
+#define AIO_SERVER      "mosquitto.beachlab.org"
+// Using port 8883 for MQTTS
+#define AIO_SERVERPORT  1883
+// MQTT Account Configuration
+#define AIO_USERNAME "fmcu"
+#define AIO_KEY      "Push 1 Button"
+
+// WiFiFlientSecure for SSL/TLS support
+WiFiClient client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+
+/****************************** Feeds ***************************************/
+
+// Setup a feed with the id for publishing.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Publish alive = Adafruit_MQTT_Publish(&mqtt, "fmcu/id");
+
+// letsencrypt root CA X3
+const char letsencrypt_root_ca[] PROGMEM = R"=====(
+-----BEGIN CERTIFICATE-----
+MIIFYDCCBEigAwIBAgIQQAF3ITfU6UK47naqPGQKtzANBgkqhkiG9w0BAQsFADA/
+MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT
+DkRTVCBSb290IENBIFgzMB4XDTIxMDEyMDE5MTQwM1oXDTI0MDkzMDE4MTQwM1ow
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwggIiMA0GCSqGSIb3DQEB
+AQUAA4ICDwAwggIKAoICAQCt6CRz9BQ385ueK1coHIe+3LffOJCMbjzmV6B493XC
+ov71am72AE8o295ohmxEk7axY/0UEmu/H9LqMZshftEzPLpI9d1537O4/xLxIZpL
+wYqGcWlKZmZsj348cL+tKSIG8+TA5oCu4kuPt5l+lAOf00eXfJlII1PoOK5PCm+D
+LtFJV4yAdLbaL9A4jXsDcCEbdfIwPPqPrt3aY6vrFk/CjhFLfs8L6P+1dy70sntK
+4EwSJQxwjQMpoOFTJOwT2e4ZvxCzSow/iaNhUd6shweU9GNx7C7ib1uYgeGJXDR5
+bHbvO5BieebbpJovJsXQEOEO3tkQjhb7t/eo98flAgeYjzYIlefiN5YNNnWe+w5y
+sR2bvAP5SQXYgd0FtCrWQemsAXaVCg/Y39W9Eh81LygXbNKYwagJZHduRze6zqxZ
+Xmidf3LWicUGQSk+WT7dJvUkyRGnWqNMQB9GoZm1pzpRboY7nn1ypxIFeFntPlF4
+FQsDj43QLwWyPntKHEtzBRL8xurgUBN8Q5N0s8p0544fAQjQMNRbcTa0B7rBMDBc
+SLeCO5imfWCKoqMpgsy6vYMEG6KDA0Gh1gXxG8K28Kh8hjtGqEgqiNx2mna/H2ql
+PRmP6zjzZN7IKw0KKP/32+IVQtQi0Cdd4Xn+GOdwiK1O5tmLOsbdJ1Fu/7xk9TND
+TwIDAQABo4IBRjCCAUIwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMCAQYw
+SwYIKwYBBQUHAQEEPzA9MDsGCCsGAQUFBzAChi9odHRwOi8vYXBwcy5pZGVudHJ1
+c3QuY29tL3Jvb3RzL2RzdHJvb3RjYXgzLnA3YzAfBgNVHSMEGDAWgBTEp7Gkeyxx
++tvhS5B1/8QVYIWJEDBUBgNVHSAETTBLMAgGBmeBDAECATA/BgsrBgEEAYLfEwEB
+ATAwMC4GCCsGAQUFBwIBFiJodHRwOi8vY3BzLnJvb3QteDEubGV0c2VuY3J5cHQu
+b3JnMDwGA1UdHwQ1MDMwMaAvoC2GK2h0dHA6Ly9jcmwuaWRlbnRydXN0LmNvbS9E
+U1RST09UQ0FYM0NSTC5jcmwwHQYDVR0OBBYEFHm0WeZ7tuXkAXOACIjIGlj26Ztu
+MA0GCSqGSIb3DQEBCwUAA4IBAQAKcwBslm7/DlLQrt2M51oGrS+o44+/yQoDFVDC
+5WxCu2+b9LRPwkSICHXM6webFGJueN7sJ7o5XPWioW5WlHAQU7G75K/QosMrAdSW
+9MUgNTP52GE24HGNtLi1qoJFlcDyqSMo59ahy2cI2qBDLKobkx/J3vWraV0T9VuG
+WCLKTVXkcGdtwlfFRjlBz4pYg1htmf5X6DYO8A4jqv2Il9DjXA6USbW1FzXSLr9O
+he8Y4IWS6wY7bCkjCWDcRQJMEhg76fsO3txE+FiYruq9RUWhiF1myv4Q6W+CyBFC
+Dfvp7OOGAN6dEOM4+qR9sdjoSYKEBpsr6GtPAQw4dy753ec5
+-----END CERTIFICATE-----
+)=====";
+
+void setup()
+{
+	Serial.begin(9600);
+  delay(10);
+
+
+    pinMode(buttonPin, INPUT_PULLUP);
+    pinMode(ledPin, OUTPUT);
+
+// Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  delay(1000);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  delay(2000);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+
+  // Set Adafruit IO's root CA
+  //client.setCACert(letsencrypt_root_ca);
+
+}
+
+
+void loop()
+{
+	Serial.print("WIFI: Connected to Wi-Fi? ");
+	if (WiFi.status() != WL_CONNECTED)
+	{
+		Serial.println("No.");
+	}
+	else
+	{
+		Serial.println("Yes.");
+
+                  	////////////////////////////////////////
+	              	// Publish to MQTT server
+                  // Ensure the connection to the MQTT server is alive (this will make the first
+                  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+                 // function definition further below.
+                 MQTT_connect();
+
+
+
+
+          buttonState = digitalRead(buttonPin);
+
+          // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+          if (buttonState == LOW) {
+                 // Now we can publish stuff!
+                Serial.print(F("\nSending id "));
+                Serial.print(id);
+                Serial.print(F(" to fmcu feed..."));
+              if (! alive.publish(id)) {
+                  Serial.println(F("Failed"));
+                } else {
+                  Serial.println(F("OK!"));
+                  digitalWrite(ledPin, HIGH);
+                  delay(500);
+                }
+            } else {
+              digitalWrite(ledPin, LOW);
+               
+          }
+
+
+
+	
+
+
+	} 
+
+
+
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+
+  Serial.println("MQTT Connected!");
+}
